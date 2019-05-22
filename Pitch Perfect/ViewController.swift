033b460a4d0eb66ab15742fbde4764e7f863973a
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-22 21:08:44 +0300

    Added recordingStatus outlet and record button action

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var recordingStatusLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func recordButton(_ sender: UIButton) {
+    }
 }
