0616e0a045ddea8438bacc6a883270ac4ffd498e
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-23 13:03:34 +0300

    Added and assigned PlaySoundsViewController

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Rinalds Domanovs on 23/05/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
